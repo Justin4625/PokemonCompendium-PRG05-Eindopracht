0255b0f080b32f1f85fc14e280b7dad6db4a0661
justin <kinhanjustin@gmail.com>
2024-10-17 14:04:59 +0200

    Log out toegevoegd

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,9 +16,19 @@
                 <x-nav-link href="/" class="hover:text-blue-400 transition duration-300">Home</x-nav-link>
                 <x-nav-link href="list" class="text-gray-300 hover:text-blue-400 transition duration-300 text-lg">Pokémon Compendium</x-nav-link> <!-- Compendium link accessible for all -->
             </div>
-            <div class="space-x-6">
+            <div class="flex items-center space-x-6"> <!-- Changed to flex to align items -->
                 @auth
-                    <!-- Add any authenticated user-specific links here -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-nav-link href="{{ route('logout') }}"
+                                    class="text-gray-300 hover:text-blue-400 transition duration-300 text-lg"
+                                    onclick="event.preventDefault();
+                             this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-nav-link>
+                    </form>
+
                 @else
                     <x-nav-link href="{{ route('login') }}" class="text-gray-300 hover:text-blue-400 transition duration-300 text-lg">Log in</x-nav-link>
 
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,8 +6,6 @@
                 <li class="bg-gray-800 p-4 rounded-lg shadow-lg">
                     <h2 class="text-xl text-gray-100 font-semibold">{{ $poke->name }}</h2>
                     <p class="text-gray-300">Type: <span class="text-blue-400">{{ $poke->type_id }}</span></p>
-                    <p class="text-gray-300">Region: <span class="text-blue-400">{{ $poke->region }}</span></p>
-                    <p class="text-gray-300">Status: <span class="text-blue-400">{{ $poke->status }}</span></p>
                     @if (Route::has('login'))
                         @auth
                             <x-nav-link href="{{ route('show', ['id' => $poke->id]) }}" class="mt-2 inline-block bg-blue-500 text-white px-4 py-1 rounded-md transition duration-300 transform hover:bg-blue-600 hover:scale-105">Details</x-nav-link>
