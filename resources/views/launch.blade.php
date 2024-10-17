40f658e4e8a81be348166941d23526957e10866c
justin <kinhanjustin@gmail.com>
2024-10-17 14:14:36 +0200

    Authenticatie details verbeterd

--- resources/views/launch.blade.php
+++ resources/views/launch.blade.php
@@ -1,6 +1,6 @@
 <x-layout></x-layout>
 
-<h1 class="text-5xl text-white font-bold mb-6 text-center">Welkom op de website!</h1>
+<h1 class="text-5xl text-white font-bold mb-6 text-center">Welcome to the website!</h1>
 
 @unless (Auth::check())
     <p class="text-gray-200 text-center">You are not signed in.</p>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,15 +1,21 @@
 <x-layout>
     <div class="container mx-auto p-8">
-        <h1 class="text-5xl text-white font-bold mb-4 text-center">{{ $poke->name }}</h1>
-        <div class="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <p class="text-lg text-gray-300">Type: <span class="text-blue-400">{{ $poke->type_id }}</span></p>
-            <p class="text-lg text-gray-300">Region: <span class="text-blue-400">{{ $poke->region }}</span></p>
-            <p class="text-lg text-gray-300">Status: <span class="text-blue-400">{{ $poke->status }}</span></p>
-        </div>
-        <div class="mt-6 text-center">
-            <a href="{{ route('list.index') }}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
-                Back to List
-            </a>
-        </div>
+        @auth
+            <h1 class="text-5xl text-white font-bold mb-4 text-center">{{ $poke->name }}</h1>
+            <div class="bg-gray-800 p-6 rounded-lg shadow-lg">
+                <p class="text-lg text-gray-300">Type: <span class="text-blue-400">{{ $poke->type_id }}</span></p>
+                <p class="text-lg text-gray-300">Region: <span class="text-blue-400">{{ $poke->region }}</span></p>
+                <p class="text-lg text-gray-300">Status: <span class="text-blue-400">{{ $poke->status }}</span></p>
+            </div>
+            <div class="mt-6 text-center">
+                <a href="{{ route('list.index') }}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
+                    Back to List
+                </a>
+            </div>
+        @else
+            <div class="bg-red-500 text-white p-4 rounded-lg text-center">
+                You need to log in to view these details.
+            </div>
+        @endauth
     </div>
 </x-layout>
