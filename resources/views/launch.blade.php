5b3614b9a874346424a47da9d46becbf06532d45
justin <kinhanjustin@gmail.com>
2024-10-22 14:07:31 +0200

    Kleine aanpassingen

--- resources/views/launch.blade.php
+++ resources/views/launch.blade.php
@@ -1,7 +1,7 @@
-<x-layout></x-layout>
-
+<x-layout>
 <h1 class="text-5xl text-white font-bold mb-6 text-center">Welcome to the website!</h1>
 
 @unless (Auth::check())
     <p class="text-gray-200 text-center">You are not signed in.</p>
 @endunless
+</x-layout>
