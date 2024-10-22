a2ba258a5792c357ebe59eb7fb2d03491132843c
justin <kinhanjustin@gmail.com>
2024-10-22 15:04:10 +0200

    Admin heeft eigen pagina

--- resources/views/launch.blade.php
+++ resources/views/launch.blade.php
@@ -4,4 +4,8 @@
 @unless (Auth::check())
     <p class="text-gray-200 text-center">You are not signed in.</p>
 @endunless
+
+    @if(auth()->check() && auth()->user()->is_admin)
+        <h1>Admin Account</h1>
+    @endif
 </x-layout>
